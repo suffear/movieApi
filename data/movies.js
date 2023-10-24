44e09229e5b4b87ec089f291f7541865f1860cad
suffear <matzniklas@gmail.com>
2023-10-24 15:57:41 +0200

    errors get displayed better

---
+++ data/movies.js
@@ -0,0 +1,204 @@
+const movies = [
+    {
+      "id": 1,
+      "title": "Inception",
+      "year": 2010,
+      "director": "Christopher Nolan",
+      "genre": "Science Fiction",
+      "rating": 8.8
+    },
+    {
+      "id": 2,
+      "title": "The Shawshank Redemption",
+      "year": 1994,
+      "director": "Frank Darabont",
+      "genre": "Drama",
+      "rating": 9.3
+    },
+    {
+      "id": 3,
+      "title": "Pulp Fiction",
+      "year": 1994,
+      "director": "Quentin Tarantino",
+      "genre": "Crime",
+      "rating": 8.9
+    },
+    {
+      "id": 4,
+      "title": "The Dark Knight",
+      "year": 2008,
+      "director": "Christopher Nolan",
+      "genre": "Action",
+      "rating": 9.0
+    },
+    {
+      "id": 5,
+      "title": "Forrest Gump",
+      "year": 1994,
+      "director": "Robert Zemeckis",
+      "genre": "Drama",
+      "rating": 8.8
+    },
+    {
+      "id": 6,
+      "title": "The Matrix",
+      "year": 1999,
+      "director": "The Wachowskis",
+      "genre": "Science Fiction",
+      "rating": 8.7
+    },
+    {
+      "id": 7,
+      "title": "Gladiator",
+      "year": 2000,
+      "director": "Ridley Scott",
+      "genre": "Action",
+      "rating": 8.5
+    },
+    {
+      "id": 8,
+      "title": "The Lord of the Rings: The Fellowship of the Ring",
+      "year": 2001,
+      "director": "Peter Jackson",
+      "genre": "Fantasy",
+      "rating": 8.8
+    },
+    {
+      "id": 9,
+      "title": "The Godfather",
+      "year": 1972,
+      "director": "Francis Ford Coppola",
+      "genre": "Crime",
+      "rating": 9.2
+    },
+    {
+      "id": 10,
+      "title": "Fight Club",
+      "year": 1999,
+      "director": "David Fincher",
+      "genre": "Drama",
+      "rating": 8.8
+    },
+    {
+      "id": 11,
+      "title": "Schindler's List",
+      "year": 1993,
+      "director": "Steven Spielberg",
+      "genre": "Biography",
+      "rating": 8.9
+    },
+    {
+      "id": 12,
+      "title": "The Lord of the Rings: The Return of the King",
+      "year": 2003,
+      "director": "Peter Jackson",
+      "genre": "Fantasy",
+      "rating": 8.9
+    },
+    {
+      "id": 13,
+      "title": "The Green Mile",
+      "year": 1999,
+      "director": "Frank Darabont",
+      "genre": "Drama",
+      "rating": 8.6
+    },
+    {
+      "id": 14,
+      "title": "Forrest Gump",
+      "year": 1994,
+      "director": "Robert Zemeckis",
+      "genre": "Drama",
+      "rating": 8.8
+    },
+    {
+      "id": 15,
+      "title": "The Matrix",
+      "year": 1999,
+      "director": "The Wachowskis",
+      "genre": "Science Fiction",
+      "rating": 8.7
+    },
+    {
+      "id": 16,
+      "title": "Gladiator",
+      "year": 2000,
+      "director": "Ridley Scott",
+      "genre": "Action",
+      "rating": 8.5
+    },
+    {
+      "id": 17,
+      "title": "The Lord of the Rings: The Fellowship of the Ring",
+      "year": 2001,
+      "director": "Peter Jackson",
+      "genre": "Fantasy",
+      "rating": 8.8
+    },
+    {
+      "id": 18,
+      "title": "The Godfather",
+      "year": 1972,
+      "director": "Francis Ford Coppola",
+      "genre": "Crime",
+      "rating": 9.2
+    },
+    {
+      "id": 19,
+      "title": "Fight Club",
+      "year": 1999,
+      "director": "David Fincher",
+      "genre": "Drama",
+      "rating": 8.8
+    },
+    {
+      "id": 20,
+      "title": "Schindler's List",
+      "year": 1993,
+      "director": "Steven Spielberg",
+      "genre": "Biography",
+      "rating": 8.9
+    },
+    {
+      "id": 21,
+      "title": "The Lord of the Rings: The Return of the King",
+      "year": 2003,
+      "director": "Peter Jackson",
+      "genre": "Fantasy",
+      "rating": 8.9
+    },
+    {
+      "id": 22,
+      "title": "Inception",
+      "year": 2010,
+      "director": "Christopher Nolan",
+      "genre": "Science Fiction",
+      "rating": 8.8
+    },
+    {
+      "id": 23,
+      "title": "Pulp Fiction",
+      "year": 1994,
+      "director": "Quentin Tarantino",
+      "genre": "Crime",
+      "rating": 8.9
+    },
+    {
+      "id": 24,
+      "title": "The Dark Knight",
+      "year": 2008,
+      "director": "Christopher Nolan",
+      "genre": "Action",
+      "rating": 9.0
+    },
+    {
+        "id": 25,
+        "title": "Memento",
+        "year": 2000,
+        "director": "Christopher Nolan",
+        "genre": "Mystery",
+        "rating": 8.4
+    }
+]
+
+module.exports = movies;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
+const app = require('../app.js');
+const movies = require('../data/movies.js');
 
 // Serve index.html for the root route
 router.get('/documentation', function(req, res, next) {
   res.render('documentation')
 });
+
+// serve movies.js for the /movies route
+router.get('/movies', function(req, res, next) {
+  res.render('data', { title: 'Movies', header: 'Movies', items: movies });
+});
+
+
+
+// Serve modified movies.js for the /top10movies route
+router.get('/top10movies', (req, res) => {
+  const sortedMovies = movies.sort((a, b) => {return b.rating - a.rating;});
+  const top10Movies = sortedMovies.slice(0, 10);
+
+  res.render('data', { title: 'Top 10 Rated Movies', header: 'Top 10 Rated Movies', items: top10Movies });
+});
+
 // Serve documentation.html for the /documentation route
 router.get('/', function(req, res, next) {
   res.render('index')
 });
 
-
+// Page not found handler
+router.get('/notfound', (req, res, next) => {
+  next(createError(404, 'Page not found'));
+});
 
 module.exports = router;
